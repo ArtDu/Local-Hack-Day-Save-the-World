fde8fe4da760c0e2d4841eb71f72671281cd1b0f
ArtDub <rusartdub@gmail.com>
2020-04-18 14:17:32 +0300

    git comments

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def hello_world():
     print(request.form)
     if 'text' in request.form:
       text = request.form['text']
+      # Ваш текст здесь
+      # можете вызвать просто здесь вызвать вашу функцию питона,
+      # которая будет всё делать с параметром text
+      # your_function(text)
     return render_template('index.html', text=text)
   except KeyError:
     return "KeyError"
